9527f720d866bb71ad43b0795dd05762acf2ef62
claraAgMd <clarabea.aguiar@gmail.com>
2023-11-19 22:20:02 -0300

    rising general structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Responsible from "./Pages/ResponsibleProfile/Responsible"
+import StepByStepVideo2 from "./Pages/StepByStep/StepByStepVideo2"
+
+
+
+
+
+function App() {
+
+    return (
+        <body>
+         
+          <StepByStepVideo2 />
+        </body>
+      
+      )
+
+}
+
+export default App
