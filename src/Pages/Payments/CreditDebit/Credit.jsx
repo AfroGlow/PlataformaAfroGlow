60f2bc671467c11cd168edee0a58defafedd853a
Marcos Vinicius <viniciusmouraa234@gmail.com>
2023-11-26 20:16:53 -0300

    purchase completed alert

--- src/Pages/Payments/CreditDebit/Credit.jsx
+++ src/Pages/Payments/CreditDebit/Credit.jsx
@@ -110,6 +110,7 @@ const Credit = () => {
     if (isFormValid()) {
       console.log('Formulário enviado:', formData);
       // Adicione aqui a lógica de envio do formulário para o servidor ou qualquer outra ação desejada
+      alert("Compra finalizada")
       window.location.reload();
     }
   };
