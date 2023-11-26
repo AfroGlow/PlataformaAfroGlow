fe3b695bcfcad01625a326e4c6b257964ecdf6b7
Marcos Vinicius <viniciusmouraa234@gmail.com>
2023-11-26 20:01:11 -0300

    payment form with validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import React from 'react'
 import Home from './Pages/Home/Home'
+import Payments from './Pages/Payments/Payments'
 
 
 function App() {
 
   return (
-    <body>
-      <Home />
-    </body>
+    <div> 
+    </div>
   )
 }
 
