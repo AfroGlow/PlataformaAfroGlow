eb785c4e43113676756de6d1ae4b14e5e440cf0c
Vitória Florêncio <vitoriaflorencio6@gmail.com>
2023-11-27 22:17:09 -0300

    Uploading login, registration, profiles, edit profile and no page pages, and image files

--- Views/src/App.jsx
+++ Views/src/App.jsx
@@ -1,13 +1,9 @@
-import ChildrenProfile from "./Pages/ChildrenProfile/ChildrenProfile"
-import Partners from "./Pages/Partners/Partners"
 import RouterApp from "./routers"
 
 function App() {
 
   return (
     <body>
-      <ChildrenProfile />
-      <Partners />
       <RouterApp />
     </body>
   )
--- Views/src/routers.jsx
+++ Views/src/routers.jsx
@@ -1,33 +1,34 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react'
-import Responsible from "./Pages/ResponsibleProfile/Responsible"
-import StepByStepVideo1 from "./Pages/StepByStep/StepByStepVideo1"
-import StepByStepVideo2 from "./Pages/StepByStep/StepByStepVideo2"
-import FamilyMedia from "./Pages/FamilyMedia/FamilyMedia"
-import Notes from './Pages/Notes/Notes'
-
-
-
-
+import ChildRegistration from "./Pages/ChildRegistration/ChildRegistration";
+import ParentRegistration from "./Pages/ParentRegistration/ParentRegistration";
+import Login from "./Pages/Login/Login";
+import Profile from "./Pages/Profiles/Profile";
+import EditProfile from "./Pages/EditProfile/EditProfile";
+import EditProfileChild from "./Pages/EditProfileChild/EditProfileChild";
+import Nopage from "./Pages/Nopage/Nopage";
+import EditProfileParents from "./Pages/EditProfileParents/EditProfileParents";
 
 
 function RouterApp() {
 
-    return (
-        <Router>
-         <Routes>
-          <Route path='/' element={< Responsible/>}>Perfil Responsável</Route> 
-          <Route path='/video1' element={< StepByStepVideo1/>}>Tutorial 1</Route> 
-          <Route path='/video2' element={< StepByStepVideo2/>}>Tutorial 2</Route> 
-          <Route path='/midias' element={< FamilyMedia/>}>Mídias em família</Route> 
-          <Route path='/notas' element={< Notes />}>Notas</Route> 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ParentRegistration />}>Cadastro</Route>
+        <Route path="/cadastroc" element={<ChildRegistration />}>Cadastro Criança</Route>
+        <Route path="/login" element={<Login />}>Login</Route>
+        <Route path="/perfil" element={<Profile />}>Perfil</Route>
+        <Route path="/editar" element={<EditProfile />}>Editar Perfil</Route>
+        <Route path="/editchild" element={<EditProfileChild />}>Editar Perfil Criança</Route>
+        <Route path="/editparents" element={<EditProfileParents />}>Editar Perfil Pais</Route>
+        <Route path="*" element={<Nopage />}>Editar Perfil</Route>
 
-          
-         </Routes>
-         
-        </Router>
-      
-      )
+      </Routes>
+
+    </Router>
+
+  )
 
 }
 
