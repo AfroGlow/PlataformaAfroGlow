1aa79ce3e85558e918343654f935f37292c1955a
Clara Aguiar <clarabea.aguiar@gmail.com>
2023-11-29 18:07:57 -0300

    climbing child profile routes

--- Views/src/Pages/GameThree/CoverThree/CoverThree.jsx
+++ Views/src/Pages/GameThree/CoverThree/CoverThree.jsx
@@ -1,10 +1,14 @@
 import './CoverThree.css';
 import Games from '../../Games/CoversGames/Game';
+import { Link } from 'react-router-dom';
+Link
 
 const CoverThree = () => {
     return(
         <section className='container-cover-three'>
-            <Games iconSound={'icon-sound-pink.svg'} titleGame= {'Cachinhos definidos'} ButtonGame={'button-game-3.svg'}/> 
+            <Link to="/tutorial3">
+                <Games iconSound={'icon-sound-pink.svg'} titleGame= {'Cachinhos definidos'} ButtonGame={'button-game-3.svg'}/> 
+            </Link>
         </section>
     )
 }
