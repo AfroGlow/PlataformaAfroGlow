fca15d28b033348d4f9f338bcb0d98bb05368551
Clara Aguiar <clarabea.aguiar@gmail.com>
2023-11-30 00:07:57 -0300

    updates

--- Views/src/Pages/pagesIA/components/ImageClassification/index.jsx
+++ Views/src/Pages/pagesIA/components/ImageClassification/index.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-
-//import * as tf from "@tensorflow/tfjs";
-
-// import * as tf from "@tensorflow/tfjs";
-
-// import modelJson from './AfroGlow_v2.json';
-//import { useDropzone } from 'react-dropzone'; 
+import React, { useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import { useDropzone } from 'react-dropzone'; 
 import './index.css'
 import InputForm from "../inputForm/input-form";
 import ModalReconhecimentoCapilar from "../modal/modal-ia";
@@ -15,11 +10,14 @@ const ImageClassification = () => {
     const [predictions, setPredictions] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    const loadModel = async () => {
+        const model = await tf.loadLayersModel('/path/to/AfroGlow_v2.json');
+        return model;
+    };
 
     const classifyImage = async (imgTensor) => {
         setIsLoading(true);
-        const model = await tf.loadLayersModel(modelJson);
-
+        const model = await loadModel();
         const predictions = await model.classify(imgTensor);
         setPredictions(predictions);
         setIsLoading(false);
@@ -28,12 +26,7 @@ const ImageClassification = () => {
     const handleImageUpload = (acceptedFiles) => {
         const file = acceptedFiles[0];
         setImage(URL.createObjectURL(file));
-        const imgElement = document.createElement("img");
-        imgElement.src = URL.createObjectURL(file);
     };
-
-
-
 
     const { getRootProps, getInputProps } = useDropzone({
         onDrop: handleImageUpload,
@@ -44,60 +37,23 @@ const ImageClassification = () => {
 
     return (
         <section className="wrapper">
-
             <div className="align-title-AI">
-
-                <div className="titleAI">
-            <h2 className="p-title-ia">Faça upload de uma foto, para nossa IA </h2>
-            <div>
-            <p className="p-title-ia p-general">analisar seu cabelo</p>
-                           </div>
-                           </div>
-
-                <h2 className="titleAI">Revolucione seu cuidado com o cabelo <br /> com a nossa IA! </h2>
-
+                {/* ... (código anterior) ... */}
             </div>
-
-            <h2 className="titleAI">Revolucione seu cuidado com o cabelo <br /> com a nossa IA! </h2>
 
             <div className="reqRes">
                 <div className="box1" {...getRootProps()}>
                     <input {...getInputProps()} />
-
-                  <div className="display-none">
-                    
-                   
-      
-                       
-                        </div>
-                   
-                    {image &&  (
-                         
-
-                    <div className="display-none">
-                        <img className="imgUp" id="imgUp" src="./upload.png" alt="upload" />
-
-
-                        <p>
-                            <p className="pYellow">Arraste</p> e solte uma imagem ou clique para <p className="pYellow">selecionar</p>
-                        </p>
-                    </div>
-
+                    {/* ... (código anterior) ... */}
                     {image && (
-
-
                         <>
-
                             <div className="img-wrapper">
                                 <img className="img-wrapper-ia" src={image} alt="Uploaded" />
                             </div>
 
-                            <div className="App">
+                            <div className="App"></div>
 
-                            </div>
-
-
-                            <button className="btn-action" onClick={classifyImage}  >
+                            <button className="btn-action" onClick={classifyImage}>
                                 {isLoading ? "Analisando..." : "Analisar"}
                             </button>
                         </>
@@ -105,23 +61,10 @@ const ImageClassification = () => {
                 </div>
 
                 <div className="box2">
-                <h3 className="title-box2-ia p-general">Resultado</h3>
-                <div className="align-row-box2-ia">
-                <p className="black-p-box2-ia p-general">Cacheado</p> <p className="purple-p-box2-ia p-general">95%</p>
-                </div>
-                <div className="align-row-box2-ia">
-                <p className="black-p-box2-ia p-general">Ondulado</p> <p className="purple-p-box2-ia p-general">5%</p>
-                </div>
-                <div className="align-row-box2-ia">
-                <p className="black-p-box2-ia p-general">Liso</p>    <p className="purple-p-box2-ia p-general">0%</p>
-                </div>
-                <div className="align-btn-box2-ia">
-                <ModalReconhecimentoCapilar/>
-                </div>
+                    <h3 className="title-box2-ia p-general">Resultado</h3>
+                    {/* ... (código anterior) ... */}
                     {predictions.length > 0 && (
                         <>
-
-                        
                             <h3 className="title">Resultado</h3>
                             <ul className="list-result">
                                 {predictions.map((prediction, index) => (
