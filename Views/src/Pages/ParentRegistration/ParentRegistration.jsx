d8ea7f70d6d84768507caaa9e05a91edd802998b
Marcos Vinicius <viniciusmouraa234@gmail.com>
2023-12-08 12:27:40 -0300

    adding CPF validation and shipping logic

--- Views/src/Pages/ParentRegistration/ParentRegistration.jsx
+++ Views/src/Pages/ParentRegistration/ParentRegistration.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import { validate } from 'cpf-check';
 import Input from "../../Components/Input/input"
 import LogoA from '/public/Logo.svg'
 import ImgDir from '/public/imagem direita.png'
@@ -8,6 +10,36 @@ import { Link } from "react-router-dom";
 
 
 function CadastroR() {
+    const [cpf, setCpf] = useState('');
+    const [isValid, setIsValid] = useState(true);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const handleCpf = (event) => {
+        let inputCpf = event.target.value.replace(/\D/g, '');
+    
+        if (inputCpf.length > 11) {
+          inputCpf = inputCpf.slice(0, 11);
+        }
+    
+        inputCpf = inputCpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    
+        setCpf(inputCpf);
+        setIsValid(validate(inputCpf));
+      };
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormSubmitted(true);
+        const cpfWithoutMask = cpf.replace(/\D/g, '');
+        setIsValid(validate(cpfWithoutMask));
+        if(isValid){
+            alert("CPF inválido. Por favor, verifique e tente novamente.")
+        } else {
+            // lógica adicional para lidar com o envio do formulário
+            window.location.href = "/cadastrocrianca";
+        }
+      };
+
     return (    
         <section className="containerSection">
 
@@ -20,14 +52,19 @@ function CadastroR() {
                 </div>
 
                 <div className="forms-register">
-                    <Input label={'Nome'} labelFor={'name'} type={'text'} id={'userName'} PlaceH={'Insira seu Nome'} />
-                    <Input label={'Nome Social'} labelFor={'socialName'} type={'text'} id={'socialName'} PlaceH={'Nome Social'} />
-                    <Input label={'E-mail'} labelFor={'email'} type={'text'} id={'email'} PlaceH={'Email'} />
-                    <InputDuo label={'CPF'} labelFor={'cpf'} type={'number'} id={'userCpf'} maxLength={'14'} PlaceH={'CPF'} label2={'Telefone'} labelFor2={'tel'} type2={'tel'} id2={'telUsuario'} PlaceH2={'Telefone'} />
-                    <Input label={'Senha'} labelFor={'password'} type={'password'} id={'userPassword'} PlaceH={'Crie uma senha'} />
-            </div>
+                    <form action="" onSubmit={handleSubmit} method='post'>
+                        <Input label={'Nome'} labelFor={'name'} type={'text'} id={'userName'} PlaceH={'Insira seu Nome'} />
+                        <Input label={'Nome Social'} labelFor={'socialName'} type={'text'} id={'socialName'} PlaceH={'Nome Social'} />
+                        <Input label={'E-mail'} labelFor={'email'} type={'text'} id={'email'} PlaceH={'Email'} />
+                        <InputDuo label={'CPF'} labelFor={'cpf'} type={'text'} id={'userCpf'} PlaceH={'CPF'} value={cpf} onChange={handleCpf} label2={'Telefone'} labelFor2={'tel'} type2={'tel'} id2={'telUsuario'} PlaceH2={'Telefone'} />
+                        {!isValid && formSubmitted && <p style={{ color: 'red' }}>CPF inválido</p>}
+                        <Input label={'Senha'} labelFor={'password'} type={'password'} id={'userPassword'} PlaceH={'Crie uma senha'} />
+                        <div className="buttonRegister">
+                            <UniversalButton Link propsBtn={'Cadastre-se'} type="submit" />
+                        </div>
+                    </form>
+                </div>
                     <div className="btn-register">
-                        <Link to="/cadastrocrianca" className="link-router"><UniversalButton Link propsBtn={'Cadastre-se'} /></Link>
                         <p className="paragrafS">Você já tem uma conta? <Link to={'/login'} className="link-router"><span className="link">Realizar login</span></Link></p>
                     </div>
                 </div>
