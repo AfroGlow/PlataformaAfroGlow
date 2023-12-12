50a378acb2f8b387ac57380130d10d1672bb5c47
Vitória Florêncio <vitoriaflorencio6@gmail.com>
2023-12-12 08:33:43 -0300

    assigning image changes to the profile of those responsible

--- Views/src/Pages/Profiles/Profile.jsx
+++ Views/src/Pages/Profiles/Profile.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom'
 import EditButton from '../../Components/ButtonEdit/btnEdit'
 import LeaveButton from '../../Components/ButtonLeave/btnLeave'
 import './Profile.css'
-
+import  ImageHandlerChild from '../EditProfileChild/ImageHandlerChild/ImageHandlerChild'
+import ImageHandlerParents from '../EditProfileParents/ImageHandler/ImageHandlerProfile'
 
 function Perfil() {
   return (
@@ -13,19 +14,19 @@ function Perfil() {
         </Link>
         <div className='info-edit'>
           <h2 className='title-page'>Quem está acessando?</h2>
-          <Link to="/editar" className='link-router'><EditButton /></Link>
+          <Link to="/editar" className='router'><EditButton /></Link>
         </div>
         <div className="profiles card-1">
-          <Link to='/perfilresponsavel'>
+          <Link to='/perfilresponsavel' style={{ textDecoration: 'none' }}>
             <div className="parents-profile">
               <h2 className='name-profile'>Página Responsável</h2>
-              <img src="../public/img-responsavel-g.png" alt="Avatar mulher parda, com cabelo crespo preto, blusa amarela e brincos" className='img-parents' />
+              <ImageHandlerParents defaultImage="perfil-avt-adult-1.png" className="img-profile-parents"/>
             </div>
           </Link>
-          <Link to='/perfilcrianca'>
+          <Link to='/perfilcrianca' style={{ textDecoration: 'none' }}>
             <div className="children-profile card-1">
               <h2 className='name-profile'>Página Criança</h2>
-              <img id='img-child' src="../public/img-criança-g.png" alt="Avatar menino negro, com cabelo cacheado e com blusa amarela" />
+              < ImageHandlerChild defaultImage="perfil-avt-1.png" className="img-profile img-profile-child" />
             </div>
           </Link>
         </div>
