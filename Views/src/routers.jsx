9b2f402b797c8c006b42710a721fca451b785fdf
Fabricio1308 <125225027+Fabricio1308@users.noreply.github.com>
2023-12-13 12:21:16 -0300

    Tidying Up Folders

--- Views/src/routers.jsx
+++ Views/src/routers.jsx
@@ -8,8 +8,8 @@ import Notes from './Pages/Notes/Notes'
 import Form2 from './Pages/pagesIA/Form2/Form2.jsx'
 import Form3 from './Pages/pagesIA/Form3/Form3.jsx'
 import Form1 from './Pages/pagesIA/Form1/Form1.jsx'
-import ContactUs from './Pages/pagesIA/pagesFaleConosco/ContactUs.jsx'
-import TelaFinal from './Pages/pagesIA/TelaFinal/TelaFinal.jsx'
+import ContactUs from './Pages/ContactUs/ContactUs.jsx'
+import CalculateShipping from './Pages/pagesIA/CalculateShipping/CalculateShipping.jsx'
 import ImageClassification from './Pages/pagesIA/components/ImageClassification/index.jsx';
 import ChildRegistration from "./Pages/ChildRegistration/ChildRegistration";
 import ParentRegistration from "./Pages/ParentRegistration/ParentRegistration";
@@ -83,7 +83,7 @@ function RouterApp() {
         <Route path="/Form1" element={<Form1 />}></Route>
         <Route path="/Form2" element={<Form2 />} ></Route>
         <Route path="/Form3" element={<Form3 />} ></Route>
-        <Route path="/TelaFinal" element={<TelaFinal />}></Route>
+        <Route path="/CalcularFrete" element={<CalculateShipping />}></Route>
         <Route path="Parceiros" element={<AllPartners />}></Route>
         <Route path='/sobrenos' element={< About/>}></Route> 
         <Route path='/perfilcrianca' element={< ChildrenProfile/>}></Route> 
